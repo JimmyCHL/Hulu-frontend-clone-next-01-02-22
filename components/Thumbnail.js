a2640c7dfa2103cce2f7530798bc3ae64a3e05f5
Jimmy Lo <83715466+JimmyCHL@users.noreply.github.com>
2022-01-02 17:42:45 -0500

    finished version

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -16,6 +16,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
         src={`${BASE_URL}${result.backdrop_path || result.poster_path}`}
         height={1080}
         width={1920}
+        alt="pictures"
       />
       <div className="p-2">
         <p className="truncate max-w-md">{result.overview}</p>
